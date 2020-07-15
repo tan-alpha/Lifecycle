b26804eb653846ee22b0f18685c3bcb99c1aef4b
tk5656 <tanishk2001@gmail.com>
2020-07-15 23:29:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Log.v("MainActivity", "onCreate");
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.v("MainActivity", "onStart");
+        }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            Log.v("MainActivity", "onResume");
+        }
+
+        @Override
+        protected void onPause() {
+            super.onPause();
+            Log.v("MainActivity", "onPause");
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.v("MainActivity", "onStop");
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Log.v("MainActivity", "onDestroy");
+        }
+    }
+
